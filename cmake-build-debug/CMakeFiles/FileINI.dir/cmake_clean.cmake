c064ad3a9ed6537466c296ded72a0d50c7144291
Gabriele Stefani <gabristefani99@gmail.com>
2018-08-28 16:27:37 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/FileINI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileINI.dir/main.cpp.o"
+  "FileINI.pdb"
+  "FileINI"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileINI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
